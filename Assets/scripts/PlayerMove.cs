ac93a74c088c0a0772d98f93066bbc9afc4a8241
Atsuko Uemura <atsukof.kt@gmail.com>
2025-11-26 00:12:40 -0800

    Add walking animation

--- Assets/scripts/PlayerMove.cs
+++ Assets/scripts/PlayerMove.cs
@@ -1,10 +1,25 @@
 using System.Diagnostics;
 using System.Numerics;
 using UnityEngine;
+using UnityEditor.Animations;
 
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 1f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    private bool isGrounded;
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +29,7 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
         Move();
     }
 
@@ -33,6 +49,22 @@ public class PlayerMove : MonoBehaviour
         {
             UnityEngine.Debug.Log("Fire pressed");
         }
+
+        //animator.SetBool("isWalkingUp", vertical > 0);
+        animator.SetBool("isRunningRight", Mathf.Abs(horizontal) > 0.01f);
+        //animator.SetBool("isWalkingLeft", horizontal < 0);
+
+        if (horizontal > 0.01f)
+        {
+            spriteRenderer.flipX = false;   // right
+        }
+        else if (horizontal < -0.01f)
+        {
+            spriteRenderer.flipX = true;    // left
+        }
+
+        animator.SetBool("isJumping", !isGrounded);
+
 
     }
 }
